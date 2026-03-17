1e8a9204cb253655eb1d3f5a21a5858a1f93ee23
Mai Yamawaki <joooooy031006@icloud.com>
2026-03-17 20:12:38 +0900

    fix

--- backend/cosaspendientes/src/main/java/com/joy/cosaspendientes/security/JwtFilter.java
+++ backend/cosaspendientes/src/main/java/com/joy/cosaspendientes/security/JwtFilter.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -12,6 +13,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtFilter extends OncePerRequestFilter{
 	private JwtService jwtService;
 
